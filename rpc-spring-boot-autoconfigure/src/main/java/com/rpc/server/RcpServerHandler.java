1eb215c28ffede80e5e977a42cc25a483363b99d
cbeann <15910076071Aa>
2021-06-20 19:27:42 +0800

    format

--- rpc-spring-boot-autoconfigure/src/main/java/com/rpc/server/RcpServerHandler.java
+++ rpc-spring-boot-autoconfigure/src/main/java/com/rpc/server/RcpServerHandler.java
@@ -7,6 +7,7 @@ import com.rpc.model.RpcResponse;
 import com.rpc.utils.String2Class;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 
 import java.lang.reflect.Method;
@@ -15,6 +16,7 @@ import java.lang.reflect.Method;
  * @author chaird
  * @create 2021-02-07 2:59
  */
+@Slf4j
 public class RcpServerHandler extends SimpleChannelInboundHandler<String> {
 
   private ApplicationContext applicationContext;
@@ -28,13 +30,13 @@ public class RcpServerHandler extends SimpleChannelInboundHandler<String> {
 
     System.out.println(msg);
 
-    //msg =
-        //"{\"methodName\":\"getId\",\"serviceName\":\"provider01\",\"requestId\":\"d53815a0\",\"parameters\":[1],\"clazzName\":\"com.service.StudentService\",\"parameterTypeStrings\":[\"java.lang.Integer\"]}";
+    // msg =
+    // "{\"methodName\":\"getId\",\"serviceName\":\"provider01\",\"requestId\":\"d53815a0\",\"parameters\":[1],\"clazzName\":\"com.service.StudentService\",\"parameterTypeStrings\":[\"java.lang.Integer\"]}";
 
     RpcRequest requestBean = JSONUtil.toBean(msg, RpcRequest.class);
 
-    //心跳逻辑
-    if (ConstantPool.HEART_BEAT.equals(requestBean.getRequestId())){
+    // 心跳逻辑
+    if (ConstantPool.HEART_BEAT.equals(requestBean.getRequestId())) {
       System.out.println("服务端接收到心跳");
       RpcResponse rpcResponse = RpcResponse.HEART_BEAT();
       String s = JSONUtil.toJsonStr(rpcResponse);
@@ -42,29 +44,29 @@ public class RcpServerHandler extends SimpleChannelInboundHandler<String> {
       return;
     }
 
-    String[] parameterTypeStrings = requestBean.getParameterTypeStrings();
-    Class<?>[] parameterTypes = String2Class.string2Class(parameterTypeStrings);
-    requestBean.setParameterTypes(parameterTypes);
+    try {
+      String[] parameterTypeStrings = requestBean.getParameterTypeStrings();
+      Class<?>[] parameterTypes = String2Class.string2Class(parameterTypeStrings);
+      requestBean.setParameterTypes(parameterTypes);
 
-    String clazzName = requestBean.getClazzName();
-    Class<?> aClass = Class.forName(clazzName);
-    Object bean = applicationContext.getBean(aClass);
-    Method method = aClass.getMethod(requestBean.getMethodName(), requestBean.getParameterTypes());
+      String clazzName = requestBean.getClazzName();
+      Class<?> aClass = Class.forName(clazzName);
+      Object bean = applicationContext.getBean(aClass);
+      Method method =
+          aClass.getMethod(requestBean.getMethodName(), requestBean.getParameterTypes());
 
-    Object invoke = method.invoke(bean, requestBean.getParameters());
+      Object invoke = method.invoke(bean, requestBean.getParameters());
 
-    RpcResponse rpcResponse = new RpcResponse();
-    rpcResponse.setRequestId(requestBean.getRequestId());
-    rpcResponse.setReturnValue(invoke);
+      RpcResponse rpcResponse = new RpcResponse();
+      rpcResponse.setRequestId(requestBean.getRequestId());
+      rpcResponse.setReturnValue(invoke);
 
-    String s = JSONUtil.toJsonStr(rpcResponse);
+      String s = JSONUtil.toJsonStr(rpcResponse);
 
-
-    ctx.channel().writeAndFlush(s);
-
-
-
-
+      ctx.channel().writeAndFlush(s);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
     // 获取请求的接口类名称
     //    String clazzName = requestBean.getClazzName();
